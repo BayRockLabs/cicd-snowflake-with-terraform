12c58f754405732fd55f66452cd7f8540ad52f9d
sudhakar-konatham <sudhakar.r@bayrocklabs.com>
2025-10-07 23:28:04 +0530

    Update Snowflake Terraform workflow and resources
    - Commented out AWS credentials configuration in the staging workflow.
    - Added OIDC token retrieval step for Snowflake authentication.
    - Adjusted warehouse size settings in Terraform resources.
    - Updated S3 backend configuration with specific bucket and role ARN.

--- staging/main.tf
+++ staging/main.tf
@@ -7,21 +7,21 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "<your-bucket-name>"
+    bucket         = "brl-terraformstate-bucket"
     key            = "terraform-staging.tfstate"
-    region         = "<bucket-region>"
+    region         = "us-east-1"
     # Optional DynamoDB for state locking. See https://developer.hashicorp.com/terraform/language/settings/backends/s3 for details.
     # dynamodb_table = "terraform-state-lock-table"
     encrypt        = true
-    role_arn       = "arn:aws:iam::<your-aws-account-no>:role/<terraform-s3-backend-access-role>"
+    role_arn       = "arn:aws:iam::103074025996:role/snowflake-demo-role"
   }
 }
 
 provider "snowflake" {
-  username    = "<your_snowflake_username>"
-  account     = "<your_snowflake_account_identifier>"
-  role        = "<your_snowflake_role>"
-  private_key = var.snowflake_private_key
+  username    = "cicduser"
+  account     = "xr96327"
+  role        = "SNOWFLAKE_LEARNING_ROLE"
+  # private_key = var.snowflake_private_key
 }
 
 module "snowflake_resources" {
